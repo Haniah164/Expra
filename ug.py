d270af87cb56ad2b9c02d05003f83ec480121734
Flavoured-Theta <160431477+Flavoured-Theta@users.noreply.github.com>
2025-05-26 16:03:41 +0200

    Add files via upload

--- ug.py
+++ ug.py
@@ -1,5 +1,30 @@
-from psychopy import visual, event, core, data, gui
+from psychopy import visual, event, core #import some libraries from PsychoPy
+from psychopy.hardware import keyboard
+import random
+random.seed()
+ 
+#config:
+win = visual.Window([1024, 768], monitor="testMonitor", units="pix")
+num_trials = 40
+agents = ["fair", "neutral", "unfair"]
+ 
+#create main textbox
+text_Main = visual.TextBox2(win=win, text = "Read the proposed offer carefully and decide if you want to accept:", pos=[10, 80])
 
-win = visual.Window([1024,768], fullscr=False, units='pix', )
+#create a keyboard component
+kb = keyboard.Keyboard()
 
-win.flip()
+
+#draw the stimuli and update the window
+while True: #this creates a never-ending loop
+    random_agents = random.choice(agents)
+    text_Main.draw()
+    win.flip()
+
+    if len(kb.getKeys()) > 0:
+        break
+    event.clearEvents()
+
+#cleanup
+win.close()
+core.quit()
