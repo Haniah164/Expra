c4cec8d732a85fbbeeeb187a5bc78666df4fcb83
Flavoured-Theta <160431477+Flavoured-Theta@users.noreply.github.com>
2025-07-01 10:19:25 +0200

    Add files via upload

--- mcq.py
+++ mcq.py
@@ -11,7 +11,7 @@ def emotion_mcq(win):
     randomized_emotions = random.sample(params.EMOTIONS, k=len(params.EMOTIONS))
     print(randomized_emotions)
     #Show emotion MCQ
-    emotion_text = 'What emotion do you feel right now?\n\n' + "\n".join(
+    emotion_text = 'What emotion do you feel right now? Press the corresponding key to answer.\n\n' + "\n".join(
         f"{i+1}: {e}" for i, e in enumerate(randomized_emotions)
     )
     emotion_question = visual.TextStim(win, text=emotion_text, color='white',
@@ -22,7 +22,7 @@ def emotion_mcq(win):
     selected_emotion = randomized_emotions[int(emotion_key) - 1]
 
     #Show confidence rating
-    confidence_text = 'How strong are you experiencing this emotion?\n(1 = low intensity to 5 = high intensity)'
+    confidence_text = 'How strong are you experiencing this emotion?\n(Press 1 = low intensity to 5 = high intensity)'
     confidence_question = visual.TextStim(win, text=confidence_text, color='white',
                                           height=30, wrapWidth=800)
     confidence_question.draw()
