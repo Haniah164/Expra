ac6fa47f2d0802d6784969235bca366230b149a1
Flavoured-Theta <160431477+Flavoured-Theta@users.noreply.github.com>
2025-07-01 09:27:08 +0200

    Add files via upload

--- ug_params.py
+++ ug_params.py
@@ -1,8 +1,14 @@
 # UG Params
 OFFER_SCREEN_TIME_SECONDS = 10
 OUTCOME_SCRREN_TIME_SECONDS = 1
-NUM_OF_CYCLES = 1
-NUM_OF_TRIALS_PER_CYCLE = 3
+NUM_OF_CYCLES = 6
+NUM_OF_TRIALS_PER_CYCLE = 4
+ONSET_TIME_MUSIC = 15
+block1 = [1, 3, 5, 2]
+block2 = [6, 1, 2, 3]
+block3 = [4, 1, 2, 3]
+block4 = [3, 1, 2, 3]
+blocks = [block1, block2, block3, block4]
 
 # General Params
 INSTRUCTION_TYPE = ["Emotion_Focused", "Neutral"]
@@ -18,7 +24,7 @@ EMOTIONS = ['Anger', 'Disgust', 'Sadness', 'Happiness', 'Neutral', 'Anxiety', 'C
 CONFIDENCE_SCALE = ['1', '2', '3', '4', '5']
 
 # Mood Induction Params
-MOOD_LIST = ["anger", "sadness", "happiness", "neutral"]
+MOOD_LIST = ["tension", "sadness", "joyful_activation","tension", "sadness", "joyful_activation"]
 MOOD_DIR = "stimuli"
 MUSIC_DURATION = 90
 LOG_FILE = "mood_log.csv"
